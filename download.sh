407a69ef4de7e06edcb8dd53027b8984d8cd3021
kuiper2000 <kuiper2000@gmail.com>
2022-08-17 11:17:03 +0800

    first commit

--- download.sh
+++ download.sh
@@ -14,3 +14,9 @@ do
 wget -c https://zenodo.org/record/6859733/files/SST_$(printf %02d ${month})_ens$(printf %02d ${ens}).nc?download=1
 done
 done
+
+wget -c https://zenodo.org/record/6969287/files/SPEAR_ATS_ensmean_cape.npz?download=1
+wget -c https://zenodo.org/record/6969287/files/SPEAR_ATS_ensmean_z925.npz?download=1
+wget -c https://zenodo.org/record/6969287/files/SPEAR_ATS_ensmean_z500.npz?download=1
+wget -c https://zenodo.org/record/6969287/files/SPEAR_ATS_ensmean_WSHR.npz?download=1
+wget -c https://zenodo.org/record/6969287/files/SPEAR_monthly_data_ivt_ensmean.npz?download=1
